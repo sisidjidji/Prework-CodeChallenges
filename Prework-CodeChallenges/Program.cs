ebd082cb5a18211ed13228ceebf9f26f445929c9
sihem azibi <s.azibi@live.fr>
2020-05-03 17:43:30 -0500

    challenge1 done

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -6,7 +6,39 @@ namespace Prework_CodeChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] newArray = new int[5];
+
+            Console.WriteLine("Please input 5 numbers. Press 'ENTER' after each number.");
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Please Enter the number that you want to check");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            for (int x=0;x<5;x++)
+            {
+              
+
+                if (newArray[x] == num)
+                {
+                    count++;
+                }
+                
+            }
+
+            var result = num * count;
+
+            Console.Write("the result is " +result);
+            Console.ReadLine();
+
+
         }
+
+
     }
 }
